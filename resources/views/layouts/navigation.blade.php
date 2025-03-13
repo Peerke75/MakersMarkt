02f93f4fcb25550ad7eb92eb0b1c3f94f3f9992f
D1L4N0 <dqdebie@gmail.com>
2025-03-13 10:25:07 +0100

    fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,7 +23,7 @@
                     </x-nav-link>
                 </div>
 
-
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :active="request()->routeIs('#')">
                         {{ __('Mijn Portfolio') }}
                     </x-nav-link>
