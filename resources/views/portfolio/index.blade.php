a356d9d4593a289b010b2740068aa76c76cc2729
Mnighter <malik11112222333344445555@gmail.com>
2025-03-17 10:12:07 +0100

    simpele aanpassingen bewerken/verwijderen icoontje

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -28,15 +28,14 @@
                         <p class="text-gray-500 text-sm">{{ $product->categorie->name }} | {{ $product->material }}</p>
                         <p class="text-gray-800 font-bold mt-2">€{{ number_format($product->price, 2) }}</p>
 
-                        <div class="flex justify-between items-center mt-4">
+                        <div class="flex justify-end items-center mt-4">
                             <a href="{{ route('portfolio.edit', $product->id) }}"
-                                class="text-blue-600 hover:text-blue-700 flex items-center space-x-2">
+                                class="text-white bg-blue-500 hover:bg-blue-700 p-2 rounded-full transition duration-200 mr-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                 </svg>
-                                <span>Bewerken</span>
                             </a>
 
                             <form action="{{ route('portfolio.destroy', $product->id) }}" method="POST"
