4555f160775202b6e4de6a11b0fd17922d923d1a
D1L4N0 <dqdebie@gmail.com>
2025-03-19 14:29:24 +0100

    krediet toegevoegd aan profieloverzicht
    hierbij de commit zodat je je krediet kan zien bij profieloverzicht

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -67,7 +67,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Profiel') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -77,7 +77,7 @@
                             <x-dropdown-link :href="route('logout')"
                                 onclick="event.preventDefault();
                                     this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Log Uit') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
