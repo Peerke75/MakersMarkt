93080b38d2e091fb74316b58818f093546ec5e53
Peerke75 <d297354@edu.curio.nl>
2025-03-20 09:41:47 +0100

    dubbele comment weggehaald

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -49,7 +49,6 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Winkelwagen -->
                 <div class="relative me-6">
